ae93d60e31245f1c6cd0d7afdfcb05ececc51cf0
Bandion-MW <norman.kroppen@googlemail.com>
2025-08-05 15:34:03 +0200

    Initialisierung
    Modelviewer rdy. ID fehlen, Wendelicht fehlt, LOD fehlen...

---
+++ strings.lua
@@ -0,0 +1,215 @@
+function data()
+return {
+	en = {
+
+		["mod_name"] = "Mak G1206",
+		["mod_desc"] = "The G1206 is a diesel hydraulic for heavy shunting and mainline service. The locomotive was developed and built in 1997 by MaK (Maschinenbau Kiel). In 1998 MaK was bought by Vossloh, since then the locomotives have been running under the Vossloh G1206.",
+
+        ["g1206_menu"] = "Mak G1206",
+        ["g1206_fake_menu"] = "Mak G1206 Fakes",
+		["g1206_desc"] = "The G1206 is a diesel hydraulic for heavy shunting and mainline service. The locomotive was developed and built in 1997 by MaK (Maschinenbau Kiel). In 1998 MaK was bought by Vossloh, since then the locomotives have been running under the Vossloh G1206.",
+
+		--- Standard ---
+        ["g1206_alpha_name"] = "G 1206 Alpha Trains (2017)",
+
+
+	},
+	de = {
+
+		["mod_name"] = "BR 210 | BR 215 | BR 216 | BR 217 | BR 218 | BR 219 (\"V 160-Familie\")",
+		["mod_desc"] = "In der Nachkriegszeit projektierte die Bundesbahn ein Neubauprogramm für einmotorige Großdieselloks für den mittleren Streckendienst. 1956 wurde die erste Vorserienmaschine in Betrieb genommen, die Serienfertigung began ab 1964, die Lokomotiven wurden im Fern-, Nah- und Güterverkehr eingesetzt. Die V 160-Familie umfasst die Baureihen 210, 215, 216 (V 160), 217 (V 162), 218, 219 (V 169) sowie die Unterbaureihen 218.8 225. Manche Serien bewährten sich mehr als andere, insgesamt hat diese Familienbaureihe aber das Bild der Streckendieselloks der Deutschen Bundesbahn geprägt. Viele Loks stehen heute noch bei verschiedenen Firmen im Dienst.",
+
+        ["br210_menu"] = "DB BR 210",
+        ["br215_menu"] = "DB BR 215",
+        ["br216_menu"] = "DB BR 216 (V 160)",
+        ["br217_menu"] = "DB BR 217 (V 162)",
+        ["br218_menu"] = "DB BR 218",
+        ["br219_menu"] = "DB BR 219 (V 169)",
+        ["dh1504_menu"] = "DH 1504 (ex BR 216)",
+
+        ["br210_fake_menu"] = "BR 210 Fakes",
+        ["br215_fake_menu"] = "BR 215 Fakes",
+        ["br216_fake_menu"] = "BR 216 (V160) Fakes",
+        ["br217_fake_menu"] = "BR 217 (V162) Fakes",
+        ["br218_fake_menu"] = "BR 218 Fakes",
+        ["br219_fake_menu"] = "BR 219 (V169) Fakes",
+        ["dh1504_menu"] = "DH 1504 (ex BR 216) Fakes",
+
+        ['fake'] = 'Fake - ',
+
+        ["v160_familie_fake"] = "Fake-Loks aktivieren?",
+        ["option_fake_v160_familie_desc"] = "Sollen die Fake-Loks (kein Licht, kein Fahrer) aktiviert werden?",
+
+		["v160_familie_privatbahn"] = "Privatbahn Lackierungen aktivieren?",
+		["option_privatbahn_v160_familie_desc"] = "Sollen die Lackierungen der Privatbahnen im Spiel aktiviert werden?",
+
+        ["v160_familie_br210"] = "Loks der BR 210 aktivieren?",
+        ["option_br210_v160_familie_desc"] = "Sollen die Loks der BR 210 aktiviert werden?",
+
+        ["v160_familie_br215"] = "Loks der BR 215 aktivieren?",
+        ["option_br215_v160_familie_desc"] = "Sollen die Loks der BR 215 aktiviert werden?",
+
+        ["v160_familie_br216"] = "Loks der BR 216 aktivieren?",
+        ["option_br216_v160_familie_desc"] = "Sollen die Loks der BR 216 aktiviert werden?",
+
+        ["v160_familie_br217"] = "Loks der BR 217 aktivieren?",
+        ["option_br217_v160_familie_desc"] = "Sollen die Loks der BR 217 aktiviert werden?",
+
+        ["v160_familie_br218"] = "Loks der BR 218 aktivieren?",
+        ["option_br218_v160_familie_desc"] = "Sollen die Loks der BR 218 aktiviert werden?",
+
+        ["v160_familie_br219"] = "Loks der BR 219 aktivieren?",
+        ["option_br219_v160_familie_desc"] = "Sollen die Loks der BR 219 aktiviert werden?",
+
+        ["v160_familie_br225"] = "Loks der BR 225 aktivieren?",
+        ["option_br225_v160_familie_desc"] = "Sollen die Loks der BR 225 aktiviert werden?",
+
+        ["v160_familie_br226"] = "Loks der BR 226 aktivieren?",
+        ["option_br226_v160_familie_desc"] = "Sollen die Loks der BR 226 aktiviert werden?",
+
+        ["v160_familie_br226"] = "Loks der Bauart DH 1504 aktivieren?",
+        ["option_br226_v160_familie_desc"] = "Sollen die Loks der DH 1504 aktiviert werden?",
+
+        ["motor_mb_839"] = 		" Diese Lok ist mit dem Motor Mercedes-Benz 839 Bb ausgestattet.",
+        ["motor_tb10_956"] = 	" Diese Lok ist mit dem Motor MTU MA 12 V 956 TB10 ausgestattet.",
+        ["motor_pa_4v"] = 		" Diese Lok ist mit dem Motor Pielstick 16 PA 4V 200 ausgestattet.",
+        ["motor_tb11_956"] = 	" Diese Lok ist mit dem Motor MTU MA 12 V 956 TB11 ausgestattet.",
+        ["motor_tb11_956_neu"] = " Diese Lok ist mit dem Motor MTU MA 12 V 956 TB11 (abgasoptimiert) ausgestattet.",
+        ["motor_r40"] = 		" Diese Lok ist mit dem Motor MTU 16 V4000 R40 ausgestattet.",
+
+        ["br210_ar_name"] = 	"BR 210 purpurrot DB (1970)",
+        ["br210_ob_name"] = 	"BR 210 ozeanblau/beige DB (1974)",
+
+		["210_desc"] = "Die BR 210 wurde ab 1970 in 8 Exemplaren gebaut, um eine stärkere Version der BR 218 zu erhalten. Nach dem erfolgreichen Test der BR 219 entschied man sich für eine stärkere Turbine als Booster. Die in sie gesetzen Erwartung erfüllte die Baureihe, allerdings war die Heizleistung im Windert zu schwach, so dass lange Züge oft in Doppeltraktion mit einer BR 218 gefahren werden mussten. Ab 1978 traten vermehrt Schäden an den Turbinen auf, da diese im Betrieb zu oft gestartet wurden, was die Lebensdauer deutlich verringerte. 1979 wurde die Gasturbinen in allen Loks ausgebaut und alle Loks als Unterbaureihe 218.9 eingereiht.",
+
+        ["br215_ar_name"] = 	"BR 215 purpurrot DB (1968)",
+        ["br215_ar2_name"] = 	"BR 215 purpurrot 2 DB (1968)",
+        ["br215_ob_name"] = 	"BR 215 ozeanblau/beige DB (1974)",
+        ["br215_ob2_name"] = 	"BR 215 ozeanblau/beige DB AG (1994)",
+        ["br215_or_name"] = 	"BR 215 orientrot DB (1987)",
+        ["br215_or2_name"] = 	"BR 215 orientrot DB AG (1994)",
+        ["br215_vr_name"] = 	"BR 215 verkehrsrot DB AG (1998)",
+        ["br215_vr2_name"] = 	"BR 215.9 verkehrsrot DB Autozug Sylt (2003)",
+
+        ["br225_ob_name"] = 	"BR 225 ozeanblau/beige DB AG (2001)",
+        ["br225_or_name"] = 	"BR 225 orientrot DB AG (2001)",
+        ["br225_vg_name"] = 	"BR 225 verkehrsgelb DB Bahnbau Gruppe (2014)",
+        ["br225_vr_name"] = 	"BR 225 verkehrsrot DB AG (2001)",
+        ["br225_vr2_name"] = 	"BR 225 verkehrsrot Railion (2003)",
+        ["br225_vr3_name"] = 	"BR 225 verkehrsrot Railion Logistics (2007)",
+
+		["215_desc"] = "Der wesentliche Unterschied der BR 215 zu den anderen verwandten Baureihen der V 160-Familie war der Dampfheizungskessel. Geplant war sie als V 163, kam aber erst ab 1968 zum Einsatz und sollte mit der Dampfheizung die Dampflokomotiven vor Personenzügen ablösen, da die BR 218 nur eine elektrische Zugheizung besaß. Die Loks kamen vor allen Arten von Zügen zum Einsatz, wurden ab dem Jahr 200 verstärkt ausgemustert. Im Jahr 2003 kamen 17 Loks zu DB AutoZug und wurden analog zur BR 225 umgebaut und als 215.9 geführt. Der Einsatz endete im Jahr 2019.",
+
+        ["225_desc"] = "Die Baureihe 225 entstand durch Umzeichnung von 68 Lokomotiven der BR 215 ab dem Jahr 2001. Es wurde sofort oder später die Dampfheizungen ausgebaut. Sie besitzen weiterhin eine Doppeltraktionssteuerung, so dass sie mit anderen Loks der Baureihe zusammen fahren können. Einige Loks wurde an private Unternehmen verkauft.",
+
+        ["v160_ar_name"] = 		"V 160 purpurrot DB (1964)",
+        ["v160_ar2_name"] = 	"V 160 purpurrot 2 DB (1964)",
+        ["br216_ar_name"] = 	"BR 216 purpurrot DB (1968)",
+        ["br216_ar2_name"] = 	"BR 216 purpurrot 2 DB (1968)",
+        ["br216_ob_name"] = 	"BR 216 ozeanblau/beige DB (1974)",
+        ["br216_ob2_name"] = 	"BR 216 ozeanblau/beige Dach ozeanblau DB (1974)",
+        ["br216_or_name"] = 	"BR 216 orientrot DB (1987)",
+        ["br216_or2_name"] = 	"BR 216 orientrot DB AG (1994)",
+        ["br216_vr_name"] = 	"BR 216 verkehrsrot DB AG (1998)",
+        ["br216_vr2_name"] = 	"BR 216 verkehrsrot DB Cargo (2003)",
+
+        ["v160-010_ar_name"] =  "V 160 010 purpurrot DB, Vorserie (1960)",
+        ["br216-010_ar_name"] =  "BR 216 010 purpurrot DB, Vorserie (1968)",
+
+        ["br226_vr_name"] = 	"BR 226 verkehrsrot xxxx (2003)",
+
+		["216_desc"] = "Die V 160, spätere BR 216, waren die ersten Vetreter der gleichnamigen Familie. Die ersten zehn Vorserienmaschinen wurden im Jahr 1960 ausgeliefert und neun davon besaßen noch ein runde Kopfform (\"Lollo\" genannt). Die zehnte Maschine erhielt eine eckigen Lokkasten, der später für die Serienmaschine übernommen wurde. Die Loks bewährten sich, die Vorserienloks wurden bis 1984 ausgemustert, in Ermangelung von Personenwagen mit Dampfheizung wurde die BR 216 ab 1998 bei DB Cargo eingesetzt, bis 2004 wurden alle Maschinen abgestellt.",
+
+        ["dh1504_niag_8_name"] = "DH 1504 NIAG 8 (2000)",
+        ["dh1504_niag_9_name"] = "DH 1504 NIAG 9 (2000)",
+        ["dh1504_mkb_name"] = "DH 1504 MKB (1998)",
+        ["dh1504_mkb_silber_name"] = "DH 1504 MKB silber (2007)",
+        ["dh1504_ohe_name"] = "DH 1504 OHE (2000)",
+        ["dh1504_blc_name"] = "DH 1504 BLC (2016)",
+
+        ["dh1504_desc"] = "Ab 1998 hat die Firma On Rail insgesamt 6 Loks umgebaut zur DH 1504. Diese war ein fast kompletter Neubau der Lok, nur der Lokkasten und die Drehgestelle wurden weitgehend übernommen. Die Loks erhielten an beiden Ende Vorbauten mit breiten Tritten und jeweils eine Funkfernsteuerung für Rangieraufgaben.",
+
+        ["v162_ar_name"] = 		"V 162 purpurrot DB (1965)",
+        ["br217_ar_name"] = 	"BR 217 purpurrot DB (1968)",
+        ["br217_ob_name"] = 	"BR 217 ozeanblau/beige DB (1974)",
+        ["br217_ob2_name"] = 	"BR 217 ozeanblau/beige DB AG (1994)",
+        ["br217_or_name"] = 	"BR 217 orientrot DB (1987)",
+        ["br217_or2_name"] = 	"BR 217 orientrot DB AG (1994)",
+        ["br217_vr_name"] = 	"BR 217 verkehrsrot DB AG (1998)",
+
+        ["br753_ob_name"] = 	"BR 753 ozeanblau/beige DB (1989)",
+        ["br753_ob2_name"] = 	"BR 753 ozeanblau/beige DB AG (1994)",
+        ["br753_pb_name"] = 	"BR 753 purpurrot/beige DB AG (2003)",
+
+		["217_desc"] = "Im Gegensatz zur BR 215 wurde die V162 (ab 1968 als BR 217 geführt) mit einer elektrischen Zugheizung in Auftrag gegeben. Sie wurde mit einem zusätzlichen Hilfsmotor ausgestattet, der im Güterverkehr auch Traktionsleistung liefern sollte. Die wenig später abgelieferten BR 218 waren aber wirtschaftlicher, so dass dann keine neuen BR 217 mehr beschafft wurden. AB 1997 kamen die Maschinen zu DB Cargo und bis Ende 2011 alle abgestellt.",
+
+        ["br218_ar_name"] = 	"BR 218 purpurrot DB (1971)",
+        ["br218_ar2_name"] = 	"BR 218 DB purpurrot 2 DB (1971)",
+        ["br218_ar3_name"] = 	"BR 218 DB AG purpurrot DB AG (1994)",
+        ["br218_ar4_name"] = 	"BR 218 DB purpurrot DB - Pielstick (1973)",
+        ["br218_ar5_name"] = 	"BR 218 DB purpurrot DB - MTU4000 R40/R41 (1973)",
+
+        ["br218_ob_name"] = 	"BR 218 ozeanblau/beige DB (1974)",
+        ["br218_ob2_name"] = 	"BR 218 ozeanblau/beige Dach ozeanblau DB (1974)",
+        ["br218_ob3_name"] = 	"BR 218 ozeanblau/beige Dach ozeanblau DB (1975)",
+        ["br218_ob4_name"] = 	"BR 218 ozeanblau/beige DB AG (1994)",
+        ["br218_ob5_name"] = 	"BR 218 ozeanblau/beige 2 DB AG (1994)",
+        ["br218_ob6_name"] = 	"BR 218 ozeanblau/beige Dach ozeanblau DB AG (2006)",
+        ["br218_ob7_name"] = 	"BR 218 ozeanblau/beige Dach ozeanblau DB AG (2025)",
+        ["br218_ob8_name"] = 	"BR 218 ozeanblau/beige DB - Pielstick (1974)",
+        ["br218_ob9_name"] = 	"BR 218 ozeanblau/beige DB - MTU4000 R40/R41 (1994)",
+        ["br218_ob10_name"] = 	"BR 218 ozeanblau/beige DB AG - MTU4000 R40/R41 (1994)",
+
+        ["br218_pb_name"] = 	"BR 218 purpurrot/beige DB (1976)",
+        ["br218_citybahn_name"] = "BR 218 kieselgrau/orange DB (1984)",
+        ["br218_citybahn2_name"] = "BR 218 kieselgrau/orange DB AG (1994)",
+        ["br218_or_name"] = 	"BR 218 orientrot DB (1987)",
+        ["br218_or2_name"] = 	"BR 218 orientrot DB AG (1994)",
+        ["br218_or3_name"] = 	"BR 218 orientrot DB AG Autozug Sylt (2003)",
+        ["br218_or5_name"] = 	"BR 218 orientrot DB - Pielstick (1987)",
+        ["br218_or6_name"] = 	"BR 218 orientrot DB AG - Pielstick (1994)",
+        ["br218_or7_name"] = 	"BR 218 orientrot DB - MTU4000 R40/R41 (1987)",
+        ["br218_or8_name"] = 	"BR 218 orientrot DB AG - MTU4000 R40/R41 (1994)",
+
+        ["br218_or_versuch_name"] = "BR 218 orientrot Versuchslackierung DB (1987)",
+        ["br218_vr_name"] = 	"BR 218 verkehrsrot DB AG (1998)",
+        ["br218_vr2_name"] = 	"BR 218 verkehrsrot 2 DB AG (1998)",
+        ["br218_vr3_name"] = 	"BR 218 verkehrsrot DB AG Nah.sh (2014)",
+        ["br218_vr4_name"] = 	"BR 218 verkehrsrot DB AG 3 Löwen Takt (2000)",
+        ["br218_vr5_name"] = 	"BR 218 verkehrsrot DB Autozug Sylt (2003)",
+        ["br218_vr6_name"] = 	"BR 218 verkehrsrot DB AG bwegt (2017)",
+        ["br218_vr7_name"] = 	"BR 218 verkehrsrot DB AG - TB10 (1998)",
+        ["br218_vr8_name"] = 	"BR 218 verkehrsrot DB AG Kempten (1998)",
+        ["br218_vr10_name"] = 	"BR 218 verkehrsrot DB AG - Pielstick (1998)",
+        ["br218_vr11_name"] = 	"BR 218 verkehrsrot DB AG - MTU4000 R40/R41 (1998)",
+
+
+        ["br218.8_vr_name"] = 	"BR 218.8 verkehrsrot Abschlepplok DB AG (2004)",
+        ["br218_vr9_name"] = 	"BR 218 440 verkehrsrot Südostbayernbahn (2013)",
+        ["br218_sbahn_ts_name"] = "BR 218 486 verkehrsrot DB AG S-Bahn Stuttgart (2015)",
+        ["br218_west_name"] = 	"BR 218 460 ozeanblau/beige DB AG Westfrankenbahn (2016)",
+        ["br218_nesa2_name"] = 	"BR 218 155 purpurrot NeSa (2018)",
+        ["br218_nesa_name"] = 	"BR 218 105 purpurrot/beige NeSa (2019)",
+        ["br218_ris_name"] = 	"BR 218 468 tiefschwarz/grau RIS (2019)",
+        ["br218_press_name"] = 	"BR 218 verkehrsblau Pressnitztalbahn (2020)",
+        ["br218_khr_name"] = 	"BR 218 117 kieselgrau/orange Kübler Heavy Rail (2021)",
+        ["br218_press2_name"] = "BR 218 verkehrsblau Pressnitztalbahn (2022)",
+        ["br218_ueg_name"] = 	"BR 218 333 minttürkis UEG (2022)",
+        ["br218_or4_name"] = 	"BR 218 406 orientrot DB AG (2023)",
+        ["br218_ice_name"] = 	"BR 218 341 ICE-Design DB AG (2023)",
+        ["br218_bke_name"] = 	"BR 218 476 ozeanblau/beige BKE (2024)",
+
+		["218_desc"] = "Die BR 218 ist eine vierachsige Diesellok und über viele Jahre hinweg war sie die häufigste und wichtigste Vertreterin der westdeutschen Streckendiesellokomotiven. Als zuletzt entwickeltes Mitglied der V 160-Lokfamilie war sie quasi das Konzentrat aus allen vorherigen Baureihen und dementsprechend bewährt hat sie sich bis heute. Die Loks wurden im Reise- und auch im Güterzugdienst eingesetzt. Im Laufe ihrer Dienstzeit hat sie alle Lackierungsvarianten der Deutschen Bundesbahn und Deutschen Bahn AG mitgemacht. Einige Loks stehen auch heute noch im Dienst bei der DB oder privaten Unternehmen.",
+
+        ["v169_ar_name"] = 		"V 169 purpurrot DB (1965)",
+        ["br219_ar_name"] = 	"BR 219 purpurrot DB (1968)",
+        ["br219_ar2_name"] = 	"BR 219 purpurrot DB (1974)",
+        ["br219_bwg_name"] = 	"BR 219 Waldhof AG (1998)",
+        ["br219_evb_name"] = 	"BR 219 EVB (2001)",
+        ["br219_lokrapid_name"] = 	"BR 219 rubinrot Lokrapid (2022)",
+
+		["219_desc"] = "Die V169 wurde gebaut als Prototyp für Lokomotiven mit einer Gasturbine als Zusatzantrieb. Ab 1968 wurde sie als Baureihe 219 eingeordnet. 1974 wurde die Gasturbine nach Schäden endgültig ausgebaut, ebenso die Heizanlage. Danach wurde sie nur noch als Güterzuglok eingesetzt und 1978 abgestellt.\nNach einem 20jährigen Aufenthalt in Italien wurde die Lok 1998 wieder nach Deutschland zurückgeholt und war als DH 280.01 bei der Bahngesellschaft Waldhof AG im Einsatz. 2001 wurde sie an die Eisenbahnen und Verkehrsbetriebe Elbe-Weser (EVB) verkauft und dort vor Containerzügen zwischen Hamburg, Bremerhaven und Bremen eingesetzt. Nach einem Unfall im Jahr 2013 wurde die Lok wiederum abgestellt. Im Jahr 2021 wurde sie an LokRapid in Oldenburg verkauft, durch den selbstständigen Lokführer Roland Sandkuhl aufgearbeitet und ist seit einer im September 2022 abgeschlossenen Hauptuntersuchung wieder vor Güterzügen im Einsatz.",
+
+	},
+}
+end
