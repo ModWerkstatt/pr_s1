263d316e972f40794a684e8da44fbc9903630f22
Bandion-MW <norman.kroppen@googlemail.com>
2025-08-11 11:33:17 +0200

    Strings und ID fix

--- strings.lua
+++ strings.lua
@@ -2,7 +2,7 @@ function data()
 return {
 	en = {
 
-		["mod_name"] = "Prussian S1 ",
+		["mod_name"] = "Prussian S1",
 		["mod_desc"] = "A total of 261 examples of the standard S 1 were built between 1885 and 1898. They were also included in the Prussian standard series. This locomotive type was developed from the P 2 and P 3.1 passenger locomotives and was intended for express train service on long, straight routes such as the Berlin–Stendal–Lehrte line. Unlike the other S 1 type, the front axle was again located behind the cylinder, and the compound design was omitted. On the Berlin-Hamburg Railway, they hauled passenger trains weighing 171 tons at a speed of 80 km/h. With the advent of more powerful locomotives, they were relegated to passenger train service. The Essen Directorate therefore classified its three locomotives as P 3.",
 
 		["pr_s1han_name"] = "S1 type Hanover",
@@ -11,21 +11,23 @@ return {
 		["pr_s1a_name"] = "S1 Normal design",
 		["pr_s1a_desc"] = "This locomotive type was developed from the P 2 and P 3.1 passenger locomotives and was intended for express train service on long, straight lines such as the Berlin–Stendal–Lehrte line. Unlike the other S 1 type, the front axle was again positioned behind the cylinder, and the compound design was omitted.",
 		
-		["pr_s1b1_name"] = "S1 Normal design",
+		["pr_s1b1_name"] = "S1 Normal design with Improved braking system",
 		["pr_s1b1_desc"] = "S1 with Improved braking system",
 		
-		["pr_s1b1_name"] = "S1 Normal design",
-		["pr_s1b1_desc"] = "S1 Improved with spark arrestor",
+		["pr_s1b2_name"] = "S1 Normal design with spark arrestor",
+		["pr_s1b2_desc"] = "S1 Improved with spark arrestor",
 		
-		["pr_s1b1_name"] = "S1 Normal design",
-		["pr_s1b1_desc"] = "Improved with side windows",
+		["pr_s1b3_name"] = "S1 Normal design with side windows",
+		["pr_s1b3_desc"] = "Improved with side windows",
 		
-		["pr_s1b1_name"] = "S1 Normal design",
-		["pr_s1b1_desc"] = "S1 Improved with lane clearer",
+		["pr_s1b4_name"] = "S1 Normal design with lane clearer",
+		["pr_s1b4_desc"] = "S1 Improved with lane clearer",
+		
+		["pr_s1_menu"] = "Prussian S1",
 	},
 	de = {
 
-		["mod_name"] = "Preußische S1 ",
+		["mod_name"] = "Preußische S1",
 		["mod_desc"] = "Von der Normalbauart der S 1 wurden in den Jahren von 1885 bis 1898 insgesamt 261 Exemplare gebaut. Sie wurden auch in die preußischen Normalien aufgenommen. Dieser Lokomotivtyp war aus den Personenzuglokomotiven der Gattungen P 2 und P 3.1 entwickelt worden und war für den Schnellzugverkehr auf langen, geraden Strecken wie zum Beispiel Berlin–Stendal–Lehrte gedacht. Im Gegensatz zum anderen Typ der S 1 war hier wieder die vordere Laufachse hinter dem Zylinder angeordnet und man hatte auf die Verbundausführung verzichtet. Bei der Berlin-Hamburger Bahn zogen sie Reisezüge von 171 Tonnen Gewicht mit einer Geschwindigkeit von 80 km/h. Mit dem Erscheinen stärkerer Lokomotiven wurden sie in den Personenzugdienst verdrängt. Die Direktion Essen stufte ihre drei Lokomotiven daher als P 3 ein.",
 
 		["pr_s1han_name"] = "S1 Bauart Hannover",
@@ -34,17 +36,19 @@ return {
 		["pr_s1a_name"] = "S1 Normalbauart",
 		["pr_s1a_desc"] = "Dieser Lokomotivtyp war aus den Personenzuglokomotiven der Gattungen P 2 und P 3.1 entwickelt worden und war für den Schnellzugverkehr auf langen, geraden Strecken wie zum Beispiel Berlin–Stendal–Lehrte gedacht. Im Gegensatz zum anderen Typ der S 1 war hier wieder die vordere Laufachse hinter dem Zylinder angeordnet und man hatte auf die Verbundausführung verzichtet.",
 		
-		["pr_s1b1_name"] = "S1 Normalbauart",
+		["pr_s1b1_name"] = "S1 Normalbauart Verbesserte Bremsanlage",
 		["pr_s1b1_desc"] = "S1 Verbesserte Bremsanlage",
 		
-		["pr_s1b1_name"] = "S1 Normalbauart",
-		["pr_s1b1_desc"] = "S1 Verbessert mit Funkenfang",
+		["pr_s1b2_name"] = "S1 Normalbauart mit Funkenfang",
+		["pr_s1b2_desc"] = "S1 Verbessert mit Funkenfang",
 		
-		["pr_s1b1_name"] = "S1 Normalbauart",
-		["pr_s1b1_desc"] = "S1 Verbessert mit Seitenfenstern",
+		["pr_s1b3_name"] = "S1 Normalbauart mit Seitenfenstern",
+		["pr_s1b3_desc"] = "S1 Verbessert mit Seitenfenstern",
 		
-		["pr_s1b1_name"] = "S1 Normalbauart",
-		["pr_s1b1_desc"] = "S1 Verbessert mit Bahnräumer",
+		["pr_s1b4_name"] = "S1 Normalbauart mit Bahnräumer",
+		["pr_s1b4_desc"] = "S1 Verbessert mit Bahnräumer",
+		
+		["pr_s1_menu"] = "Preußische S1",
 	},
 }
 end
