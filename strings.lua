b71539e2652d4b039884c639baec5f8e0b489d1a
Florian Thiel <flth@flth.de>
2025-11-28 23:23:26 +0100

    Strings überarbeitet

--- strings.lua
+++ strings.lua
@@ -10,19 +10,19 @@ return {
 
 		["pr_s1a_name"] = "S1 Normal design",
 		["pr_s1a_desc"] = "This locomotive type was developed from the P 2 and P 3.1 passenger locomotives and was intended for express train service on long, straight lines such as the Berlin–Stendal–Lehrte line. Unlike the other S 1 type, the front axle was again positioned behind the cylinder, and the compound design was omitted.",
-		
+
 		["pr_s1b1_name"] = "S1 Normal design with Improved braking system",
-		["pr_s1b1_desc"] = "S1 with Improved braking system",
-		
+		["pr_s1b1_desc"] = "This locomotive type was developed from the P 2 and P 3.1 passenger locomotives and was intended for express train service on long, straight lines such as the Berlin–Stendal–Lehrte line. Unlike the other S 1 type, the front axle was again positioned behind the cylinder, and the compound design was omitted. The Carpenter brake was replaced by a Westinghouse brake, and the air pump was installed next to the smoke chamber.",
+
 		["pr_s1b2_name"] = "S1 Normal design with spark arrestor",
-		["pr_s1b2_desc"] = "S1 Improved with spark arrestor",
-		
+		["pr_s1b2_desc"] = "This locomotive type was developed from the P 2 and P 3.1 passenger locomotives and was intended for express train service on long, straight lines such as the Berlin–Stendal–Lehrte line. Unlike the other S 1 type, the front axle was again positioned behind the cylinder, and the compound design was omitted. The chimney was fitted with a large spark arrestor.",
+
 		["pr_s1b3_name"] = "S1 Normal design with side windows",
-		["pr_s1b3_desc"] = "Improved with side windows",
-		
+		["pr_s1b3_desc"] = "This locomotive type was developed from the P 2 and P 3.1 passenger locomotives and was intended for express train service on long, straight lines such as the Berlin–Stendal–Lehrte line. Unlike the other S 1 type, the front axle was again positioned behind the cylinder, and the compound design was omitted. The driver's cab was protected from the weather by glass windows on the sides.",
+
 		["pr_s1b4_name"] = "S1 Normal design with lane clearer",
-		["pr_s1b4_desc"] = "S1 Improved with lane clearer",
-		
+		["pr_s1b4_desc"] = "This locomotive type was developed from the P 2 and P 3.1 passenger locomotives and was intended for express train service on long, straight lines such as the Berlin–Stendal–Lehrte line. Unlike the other S 1 type, the front axle was again positioned behind the cylinder, and the compound design was omitted. Version with front rail scraper.",
+
 		["pr_s1_menu"] = "Prussian S1",
 	},
 	de = {
@@ -31,23 +31,23 @@ return {
 		["mod_desc"] = "Von der Normalbauart der S 1 wurden in den Jahren von 1885 bis 1898 insgesamt 261 Exemplare gebaut. Sie wurden auch in die preußischen Normalien aufgenommen. Dieser Lokomotivtyp war aus den Personenzuglokomotiven der Gattungen P 2 und P 3.1 entwickelt worden und war für den Schnellzugverkehr auf langen, geraden Strecken wie zum Beispiel Berlin–Stendal–Lehrte gedacht. Im Gegensatz zum anderen Typ der S 1 war hier wieder die vordere Laufachse hinter dem Zylinder angeordnet und man hatte auf die Verbundausführung verzichtet. Bei der Berlin-Hamburger Bahn zogen sie Reisezüge von 171 Tonnen Gewicht mit einer Geschwindigkeit von 80 km/h. Mit dem Erscheinen stärkerer Lokomotiven wurden sie in den Personenzugdienst verdrängt. Die Direktion Essen stufte ihre drei Lokomotiven daher als P 3 ein.",
 
 		["pr_s1han_name"] = "S1 Bauart Hannover",
-		["pr_s1han_desc"] = "Die erste Serie der S 1 war 1884 die erste Verbundmaschine für den Schnellzugverkehr, die für die Preußischen Staatseisenbahnen hergestellt wurde. Die Zylinder, welche die zweite Kuppelachse antrieben, waren zwischen der Führungsachse und der ersten Kuppelachse angeordnet. Aufgrund dieser Anordnung hatten die Maschinen einen sehr ruhigen Lauf. Bei den meisten anderen deutschen 1'B-Lokomotiven (z. B. der Sächsischen III) befanden sich die Zylinder dagegen vor dem Laufradsatz. Es wurden von dieser Bauart insgesamt 14 Lokomotiven in den Jahren 1884 bis 1887 von Hanomag und Henschel gebaut.",
+		["pr_s1han_desc"] = "Die erste Serie der S 1 war 1884 die erste Verbundmaschine für den Schnellzugverkehr, die für die Preußischen Staatseisenbahnen hergestellt wurde. Die Zylinder, welche die zweite Kuppelachse antrieben, waren zwischen der Führungsachse und der ersten Kuppelachse angeordnet. Aufgrund dieser Anordnung hatten die Maschinen einen sehr ruhigen Lauf. Bei den meisten anderen deutschen 1'B-Lokomotiven befanden sich die Zylinder dagegen vor dem Laufradsatz. Es wurden von dieser Bauart insgesamt 14 Lokomotiven in den Jahren 1884 bis 1887 von Hanomag und Henschel gebaut.",
 
-		["pr_s1a_name"] = "S1 Normalbauart",
+		["pr_s1a_name"] = "S1 Bauart Magdeburg",
 		["pr_s1a_desc"] = "Dieser Lokomotivtyp war aus den Personenzuglokomotiven der Gattungen P 2 und P 3.1 entwickelt worden und war für den Schnellzugverkehr auf langen, geraden Strecken wie zum Beispiel Berlin–Stendal–Lehrte gedacht. Im Gegensatz zum anderen Typ der S 1 war hier wieder die vordere Laufachse hinter dem Zylinder angeordnet und man hatte auf die Verbundausführung verzichtet.",
-		
-		["pr_s1b1_name"] = "S1 Normalbauart Verbesserte Bremsanlage",
-		["pr_s1b1_desc"] = "S1 Verbesserte Bremsanlage",
-		
-		["pr_s1b2_name"] = "S1 Normalbauart mit Funkenfang",
-		["pr_s1b2_desc"] = "S1 Verbessert mit Funkenfang",
-		
-		["pr_s1b3_name"] = "S1 Normalbauart mit Seitenfenstern",
-		["pr_s1b3_desc"] = "S1 Verbessert mit Seitenfenstern",
-		
-		["pr_s1b4_name"] = "S1 Normalbauart mit Bahnräumer",
-		["pr_s1b4_desc"] = "S1 Verbessert mit Bahnräumer",
-		
+
+		["pr_s1b1_name"] = "S1 Bauart Magdeburg Verbesserte Bremsanlage",
+		["pr_s1b1_desc"] = "Dieser Lokomotivtyp war aus den Personenzuglokomotiven der Gattungen P 2 und P 3.1 entwickelt worden und war für den Schnellzugverkehr auf langen, geraden Strecken wie zum Beispiel Berlin–Stendal–Lehrte gedacht. Im Gegensatz zum anderen Typ der S 1 war hier wieder die vordere Laufachse hinter dem Zylinder angeordnet und man hatte auf die Verbundausführung verzichtet. Die Carpenter-Bremse wurde durch eine Westinghouse-Bremse ersetzt, die Luftpumpe wurde dabei neben der Rauchkammer angebracht.",
+
+		["pr_s1b2_name"] = "S1 Bauart Magdeburg mit Funkenfang",
+		["pr_s1b2_desc"] = "Dieser Lokomotivtyp war aus den Personenzuglokomotiven der Gattungen P 2 und P 3.1 entwickelt worden und war für den Schnellzugverkehr auf langen, geraden Strecken wie zum Beispiel Berlin–Stendal–Lehrte gedacht. Im Gegensatz zum anderen Typ der S 1 war hier wieder die vordere Laufachse hinter dem Zylinder angeordnet und man hatte auf die Verbundausführung verzichtet. Der Schornstein wurde mit einem großen Funkenfänger ausgestattet.",
+
+		["pr_s1b3_name"] = "S1 Bauart Magdeburg mit Seitenfenstern",
+		["pr_s1b3_desc"] = "Dieser Lokomotivtyp war aus den Personenzuglokomotiven der Gattungen P 2 und P 3.1 entwickelt worden und war für den Schnellzugverkehr auf langen, geraden Strecken wie zum Beispiel Berlin–Stendal–Lehrte gedacht. Im Gegensatz zum anderen Typ der S 1 war hier wieder die vordere Laufachse hinter dem Zylinder angeordnet und man hatte auf die Verbundausführung verzichtet. Der Führerstand war seitlich mit Glasfenstern gegen die Witterung geschützt.",
+
+		["pr_s1b4_name"] = "S1 Bauart Magdeburg mit Bahnräumer",
+		["pr_s1b4_desc"] = "Dieser Lokomotivtyp war aus den Personenzuglokomotiven der Gattungen P 2 und P 3.1 entwickelt worden und war für den Schnellzugverkehr auf langen, geraden Strecken wie zum Beispiel Berlin–Stendal–Lehrte gedacht. Im Gegensatz zum anderen Typ der S 1 war hier wieder die vordere Laufachse hinter dem Zylinder angeordnet und man hatte auf die Verbundausführung verzichtet. Ausführung mit vorderem Bahnräumer.",
+
 		["pr_s1_menu"] = "Preußische S1",
 	},
 }
